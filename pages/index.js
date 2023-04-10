5e90f5b3995c966876a7b00d05bf952378231e2d
cfeeney5 <c.feeney5@gmail.com>
2023-04-10 18:49:26 +0100

    Added input to dock at bottom

--- pages/index.js
+++ pages/index.js
@@ -204,58 +204,58 @@ export default function Home() {
             })}
           </div>
         </div>
-        <div className={styles.center}>
-          <div className={styles.cloudform}>
-            <form onSubmit={handleSubmit}>
-              <textarea
-                disabled={loading}
-                onKeyDown={handleEnter}
-                ref={textAreaRef}
-                autoFocus={false}
-                rows={1}
-                maxLength={512}
-                type="text"
-                id="userInput"
-                name="userInput"
-                placeholder={
-                  loading
-                    ? "Waiting for response..."
-                    : "Type your farming question..."
-                }
-                value={userInput}
-                onChange={(e) => setUserInput(e.target.value)}
-                className={styles.textarea}
-              />
-              <button
-                type="submit"
-                disabled={loading}
-                className={styles.generatebutton}
-              >
-                {loading ? (
-                  <div className={styles.loadingwheel}>
-                    <CircularProgress color="inherit" size={20} />{" "}
-                  </div>
-                ) : (
-                  // Send icon SVG in input field
-                  <svg
-                    viewBox="0 0 20 20"
-                    className={styles.svgicon}
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                  </svg>
-                )}
-              </button>
-            </form>
-          </div>
+        <div className={styles.cloudform}>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              disabled={loading}
+              onKeyDown={handleEnter}
+              ref={textAreaRef}
+              autoFocus={false}
+              rows={1}
+              maxLength={512}
+              type="text"
+              id="userInput"
+              name="userInput"
+              placeholder={
+                loading
+                  ? "Waiting for response..."
+                  : "Type your farming question..."
+              }
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+              className={styles.textarea}
+            />
+            <button
+              type="submit"
+              disabled={loading}
+              className={styles.generatebutton}
+            >
+              {loading ? (
+                <div className={styles.loadingwheel}>
+                  <CircularProgress color="inherit" size={20} />{" "}
+                </div>
+              ) : (
+                // Send icon SVG in input field
+                <svg
+                  viewBox="0 0 20 20"
+                  className={styles.svgicon}
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                </svg>
+              )}
+            </button>
+          </form>
+        </div>
+        {/* <div className={styles.center}>
           <div className={styles.footer}>
-            {/* <p>
+            <p>
               <a href="https://www.marteye.ie/privacy-policy" target="_blank">
                 Privacy Policy
               </a>
-            </p> */}
+            </p>
           </div>
-        </div>
+        </div> */}
       </main>
     </>
   );
